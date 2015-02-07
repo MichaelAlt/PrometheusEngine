4b7fdb891567b1dc1a20e6ae63b4d7ff36b1e664
Alt, Michael <m.alt@rational-online.com>
2015-02-07 11:25:49 +0100

    0.0.1.0

--- doc/install.sql
+++ doc/install.sql
@@ -1,10 +1,13 @@
 DROP DATABASE IF EXISTS prometheus;
-CREATE DTABASE IF NOT EXISTS prometheus;
+CREATE DATABASE IF NOT EXISTS prometheus;
 
 USE prometheus;
 
 CREATE TABLE IF NOT EXISTS prometheus_configuration (
-	entry_key VARCHAR(255),
+	entry_key VARCHAR(36),
+	entry_name VARCHAR(255),
 	entry_value VARCHAR(255),
-	PRIMARY KEY(entry_key)
+	PRIMARY KEY(entry_key),
+	UNIQUE KEY(entry_name),
+	INDEX(entry_name)
 )
--- src/com/prometheus/engine/PrometheusEngine.java
+++ src/com/prometheus/engine/PrometheusEngine.java
@@ -1,16 +1,131 @@
 package com.prometheus.engine;
 
-import com.prometheus.core.configuration.ConfigurationContext;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Service;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.ContextConfig;
+import org.apache.catalina.startup.Tomcat;
+
+import com.prometheus.core.configuration.ConfigurationContext;
+import com.prometheus.core.database.DatabaseManager;
+import com.prometheus.core.exception.ConfigurationException;
+import com.prometheus.core.modules.ModuleConfiguration;
+import com.prometheus.core.modules.ModuleManager;
+import com.prometheus.core.modules.annotations.ModuleType.ModuleTypes;
+
+/**
+ * 
+ * @author alt
+ *
+ */
 public class PrometheusEngine {
 
 	/**
 	 * 
 	 * @param args
+	 * @throws ServletException
+	 * @throws LifecycleException
+	 * @throws ConfigurationException
 	 */
 	public static void main(String[] args) {
+		PrometheusEngine engine = new PrometheusEngine();
+	}
 
-		ConfigurationContext.get("test");
+	/**
+	 * 
+	 * @throws ServletException
+	 * @throws LifecycleException
+	 * @throws ConfigurationException
+	 */
+	public PrometheusEngine() {
+
+		System.setProperty("user.dir", "C:/Users/alt/Documents/GitHub/PrometheusEngine");
+		
+		try {
+
+			DatabaseManager databaseManager = new DatabaseManager();
+			ModuleManager moduleManager = new ModuleManager();
+
+			Connector connector = getConnector();
+
+			Tomcat tomcat = new Tomcat();
+			tomcat.setBaseDir(System.getProperty("user.dir") + "/");
+			tomcat.setConnector(connector);
+			tomcat.enableNaming();
+			tomcat.setSilent(true);
+
+			Service service = tomcat.getService();
+			service.addConnector(connector);
+
+			Context defaultContext = tomcat.addWebapp("/", System.getProperty("user.dir") + "/web/");
+			defaultContext.addLifecycleListener(new ContextConfig());
+
+			databaseManager.getConnections(defaultContext);
+
+			for (ModuleConfiguration module : moduleManager.getModules()) {
+
+				if (module.getModuleType() == ModuleTypes.API) {
+					Tomcat.addServlet(defaultContext, module.getModuleName(), module.getModuleClass());
+					defaultContext.addServletMapping("/api/" + module.getModuleName(), module.getModuleName());
+				} else if (module.getModuleType() == ModuleTypes.WEB) {
+					Tomcat.addServlet(defaultContext, module.getModuleName(), module.getModuleClass());
+					defaultContext.addServletMapping("/" + module.getModuleName(), module.getModuleName());
+				}
+			}
+
+			tomcat.start();
+			tomcat.getServer().await();
+
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		} catch (LifecycleException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private static Connector getConnector() {
+
+		Connector connector = new Connector();
+
+		connector.setPort(Integer.valueOf((String) ConfigurationContext.get("server.port")));
+		connector.setAttribute("server", "Prometheus");
+
+		connector.setScheme("http");
+		//
+		// connector.setSecure(true);
+		// connector.setScheme("https");
+		// if (((String)
+		// configuration.get("ssl.keystore.path")).startsWith("/")) {
+		// connector.setAttribute("keystoreFile",
+		// configuration.get("ssl.keystore.path"));
+		// } else {
+		// connector.setAttribute("keystoreFile", System.getProperty("user.dir")
+		// + configuration.get("ssl.keystore.path"));
+		// }
+		// connector.setAttribute("keystorePass",
+		// configuration.get("ssl.keystore.pass"));
+		// connector.setAttribute("keyAlias",
+		// configuration.get("ssl.keystore.alias"));
+		// connector.setAttribute("clientAuth", "false");
+		// connector.setAttribute("sslProtocol", "TLS");
+		// connector.setAttribute("SSLEnabled", true);
+
+		return connector;
 	}
 
 }
